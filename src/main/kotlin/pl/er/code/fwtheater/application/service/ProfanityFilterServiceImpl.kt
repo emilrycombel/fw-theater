28048270a373a32d846ec6c5364c6d84d38427a9
emil.rycombel <emil.rycombel@er-code.net>
2024-12-18 10:41:03 +0100

    FT10: Added profanity checks for the comments
    + profanity filter has been used for simple check of bad word occurence

--- src/main/kotlin/pl/er/code/fwtheater/application/service/ProfanityFilterServiceImpl.kt
+++ src/main/kotlin/pl/er/code/fwtheater/application/service/ProfanityFilterServiceImpl.kt
@@ -1,11 +1,12 @@
 package pl.er.code.fwtheater.application.service
 
+import com.modernmt.text.profanity.ProfanityFilter
 import pl.er.code.fwtheater.domain.service.ProfanityService
 
-class ProfanityFilterServiceImpl : ProfanityService {
+class ProfanityFilterServiceImpl(private val profanityFilter: ProfanityFilter) : ProfanityService {
 
 
     override fun hasProfanedText(text: String): Boolean {
-        return false
+        return profanityFilter.test("pl", text) || profanityFilter.test("en", text) || profanityFilter.test("de", text)
     }
 }
